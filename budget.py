aa35859e4810993225c566e003acd5ca3600a4ed
Beatrix-droid <amilcare8@gmail.com>
2021-06-21 14:07:53 +0100

    completed ledger task

--- budget.py
+++ budget.py
@@ -1,3 +1,8 @@
+"""This is a budget app. It instantiates objects based on different budget
+categories like food, clothng and entertainment. When objects are created,
+they are passed in the name of the category. There is also an instance
+variable that is a list"""
+
 
 
 class Category:
@@ -8,7 +13,10 @@ class Category:
         self.balance = 0
 
 
-    def deposit(self, deposit_amt, deposit_descrpt = False):
+    def deposit(self, deposit_amt, deposit_descrpt=False):
+
+        """ This function accepts an ammount and a description for an item.
+        If no description is given, it defaults to an empty string"""
         self.deposit_amt = deposit_amt
         self.deposit_descrpt = deposit_descrpt
         self.balance = 0 + deposit_amt
@@ -20,19 +28,26 @@ class Category:
             self.ledger.append(x)
 
     def get_balance(self):
+        """ returns the current balance after withdraws, transfers and deposits have
+        taken place"""
         return float(self.balance)
 
     def check_funds(self, amt):
+        """ checks funds to see if a withdraw can happen based on the available balance"""
         self.amt = amt
         if self.amt > self.get_balance():
             return False
         return True
 
-    def withdraw(self, withdraw_amt, withdraw_descrpt = False):
+    def withdraw(self, withdraw_amt, withdraw_descrpt=False):
+        """ similar to the deposit method. Withdraw accepts an ammount and an optional
+        descritpion, and if there are enough funds, withdraws the desired amount from
+        the available balance"""
         self.withdraw_amt = withdraw_amt
         self.withdraw_descrpt = withdraw_descrpt
         if self.withdraw_descrpt:
-            ledger_item = {"description": f"{self.withdraw_descrpt}", "amount": f"{format((-1) * self.withdraw_amt,'.2f')}"}
+            ledger_item = {"description": f"{self.withdraw_descrpt}", "amount": \
+            f"{format((-1) * self.withdraw_amt,'.2f')}"}
 
         if not self.withdraw_descrpt:
             ledger_item = {"description": " ", "amount": f"{format((-1) * self.withdraw_amt,'.2f')}"}
@@ -47,17 +62,23 @@ class Category:
         return self.item
 
     def transfer(self, transfer_amt, item2):
+        """ trasnsfers a desired amount of money from one category to another"""
         Category.item = item2
         self.transfer_amt = transfer_amt
         if transfer_amt < self.deposit_amt:
-            self.ledger.append({"description": f"Transfer to {item2.item}", "amount": f"{format((-1) * self.transfer_amt, '.2f')}"})
-            item2.ledger.append({"description": f"Transfer from {self.item}", "amount": f"{format(self.transfer_amt, '.2f')}"})
+            self.ledger.append({"description": f"Transfer to {item2.item}", "amount": \
+            f"{format((-1) * self.transfer_amt, '.2f')}"})
+            item2.ledger.append({"description": f"Transfer from {self.item}", "amount": \
+            f"{format(self.transfer_amt, '.2f')}"})
             self.balance = self.deposit_amt - self.transfer_amt
-            #item2.balance = item2.balance + self.transfer_amt
+            item2.deposit_amt = self.transfer_amt
+            item2.balance = item2.deposit_amt + self.transfer_amt
             return True
         return False
 
     def __str__(self):
+        """ prints out the ledger list in a neat formatted way, so that it is easier
+        to understand for the end user"""
         stars = (self.item.center(30, "*"))
         ledger_items = ""
         for dictionary in self.ledger:
@@ -67,7 +88,7 @@ class Category:
                 words = dictionary["description"]
                 cut_words = (words[0:23])
 
-            ledger_items = ledger_items + f"{cut_words.ljust(len(stars))} {numbers.rjust(len(stars))}  \n"
+            ledger_items = ledger_items + f"{cut_words} {numbers.rjust(len(stars) - len(cut_words) -1, ' ')}  \n"
         total = f"Total: {format(self.get_balance(), '.2f')}"
 
 
@@ -92,4 +113,3 @@ auto.withdraw(15)
 
 print(food)
 print(clothing)
-
